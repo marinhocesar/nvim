4ebe4abc3ecb9881c0f3cb13d1ee0354114d11a5
marinhocesar <marinhocaiocesar@gmail.com>
2024-04-20 12:09:51 -0300

    add lsp clangd

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,7 +17,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer', 'pylsp'},
+  ensure_installed = {'tsserver', 'rust_analyzer', 'pylsp', 'clangd'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
